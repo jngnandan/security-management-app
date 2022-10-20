e7a4c1b5deb2f8cdb3ffc94788fcdf117bcef9d0
jngnandan <jngnandan@gmail.com>
2022-10-20 18:46:37 +0100

    data added in charts

--- src/views/Dashboard/PieChart.js
+++ src/views/Dashboard/PieChart.js
@@ -3,29 +3,58 @@ import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
+import { useContext, useState, useEffect } from 'react'
+// import { ContentContext } from '../../src/context/ContentContext.js'
+
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+// useEffect (() => {
+//         const {employees} = useContext(ContentContext)
+//         const {data} = employees
+        
+//         const labels = data.map((item) => item.name)
+//         const values = data.map((item) => item.salary)
+
+//         // setChartData({
+//         //     labels: labels,
+//         //     datasets: [
+//         //         {
+//         //             label: 'Salary',
+//         //             data: values,
+//         //             backgroundColor: [
+//         //                 'rgba(255, 99, 132, 0.6)',
+//         //                 'rgba(54, 162, 235, 0.6)',
+//         //                 'rgba(255, 206, 86, 0.6)',
+//         //                 'rgba(75, 192, 192, 0.6)',
+//         //                 'rgba(153, 102, 255, 0.6)',
+//         //                 'rgba(255, 159, 64, 0.6)',
+//         //                 'rgba(255, 99, 132, 0.6)'
+//         //             ]
+//         //         }
+//         //     ]
+//         // })
+//     }, [])
+            
+        
+
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['Security Guarding', 'Door Supervision', 'Close Protection', 'KeyHolding',],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5,],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
         'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
       ],
       borderWidth: 1,
     },
